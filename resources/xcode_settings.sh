38175006036c809ea2a7e3da92c9ae345686bc5b
Francisco Diaz <patchdiaz@gmail.com>
2018-07-17 13:03:33 -0700

    Exit immediately if there are errors

--- resources/xcode_settings.sh
+++ resources/xcode_settings.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 defaults write com.apple.dt.Xcode DVTTextEditorTrimTrailingWhitespace -bool YES
 defaults write com.apple.dt.Xcode DVTTextEditorTrimWhitespaceOnlyLines -bool YES
 
