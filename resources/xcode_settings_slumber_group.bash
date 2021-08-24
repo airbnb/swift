be7d35529fbbfc9a5bdfe49e387f0f552a07d6b9
waroo <waroo@users.noreply.github.com>
2021-08-24 14:20:21 -0400

    Create xcode_settings_slumber_group.bash

---
+++ resources/xcode_settings_slumber_group.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+defaults write com.apple.dt.Xcode DVTTextEditorTrimTrailingWhitespace -bool YES
+defaults write com.apple.dt.Xcode DVTTextEditorTrimWhitespaceOnlyLines -bool YES
+
+defaults write com.apple.dt.Xcode DVTTextIndentTabWidth -int 4
+defaults write com.apple.dt.Xcode DVTTextIndentWidth -int 4
+
+defaults write com.apple.dt.Xcode DVTTextPageGuideLocation -int 120
