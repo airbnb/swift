0f2a657e9d6acd68bca86bc068a295f4cdb2053f
Cal Stephens <cal.stephens@airbnb.com>
2021-08-25 08:25:25 -0700

    Enable SwiftFormat wrap rule, with a max width of 130 characters

--- resources/xcode_settings.bash
+++ resources/xcode_settings.bash
@@ -8,4 +8,5 @@ defaults write com.apple.dt.Xcode DVTTextEditorTrimWhitespaceOnlyLines -bool YES
 defaults write com.apple.dt.Xcode DVTTextIndentTabWidth -int 2
 defaults write com.apple.dt.Xcode DVTTextIndentWidth -int 2
 
-defaults write com.apple.dt.Xcode DVTTextPageGuideLocation -int 100
+defaults write com.apple.dt.Xcode DVTTextShowPageGuide -bool true
+defaults write com.apple.dt.Xcode DVTTextPageGuideLocation -int 130
