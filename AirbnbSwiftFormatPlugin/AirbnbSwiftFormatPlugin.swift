217bf06ce920e196dd99d925783c502723c1664e
Cal Stephens <cal@calstephens.tech>
2022-07-17 19:27:45 -0700

    Add AirbnbSwiftFormatPlugin

---
+++ AirbnbSwiftFormatPlugin/AirbnbSwiftFormatPlugin.swift
@@ -0,0 +1,36 @@
+import Foundation
+import PackagePlugin
+
+/// A Swift Package Manager `CommandPlugin` that executes `AirbnbSwiftFormatTool`
+/// to format source code in Swift package targets according to the Airbnb Swift Style Guide.
+@main
+struct AirbnbSwiftFormatPlugin: CommandPlugin {
+
+  func performCommand(context: PluginContext, arguments: [String]) async throws {
+    let process = Process()
+    process.launchPath = try context.tool(named: "AirbnbSwiftFormatTool").path.string
+
+    var processArguments = context.package.targets.map { $0.directory.string } + [
+      "--swift-format-path",
+      try context.tool(named: "swiftformat").path.string,
+      "--swift-lint-path",
+      try context.tool(named: "swiftlint").path.string,
+      // The process we spawn doesn't have read/write access to the default
+      // cache file locations, so we pass in our own cache paths from
+      // the plugin's work directory.
+      "--swift-format-cache-path",
+      context.pluginWorkDirectory.string + "/swiftformat.cache",
+      "--swift-lint-cache-path",
+      context.pluginWorkDirectory.string + "/swiftlint.cache",
+    ]
+
+    if arguments.contains("--lint") {
+      processArguments += ["--lint"]
+    }
+
+    process.arguments = processArguments
+    try process.run()
+    process.waitUntilExit()
+  }
+
+}
---
+++ AirbnbSwiftFormatTool/AirbnbSwiftFormatTool.swift
@@ -0,0 +1,84 @@
+import ArgumentParser
+import Foundation
+
+/// A command line tool that formats the given directories using SwiftFormat and SwiftLint,
+/// based on the Airbnb Swift Style Guide
+@main
+struct AirbnbSwiftFormatTool: ParsableCommand {
+
+  // MARK: Internal
+
+  @Argument(help: "The directories to format")
+  var directories: [String]
+
+  @Option(help: "The abosolute path to a SwiftFormat binary")
+  var swiftFormatPath: String
+
+  @Option(help: "The path to use for SwiftFormat's cache")
+  var swiftFormatCachePath: String?
+
+  @Option(help: "The abosolute path to a SwiftLint binary")
+  var swiftLintPath: String
+
+  @Option(help: "The path to use for SwiftLint's cache")
+  var swiftLintCachePath: String?
+
+  @Flag(help: "When true, source files are not reformatted")
+  var lint = false
+
+  @Option(help: "The absolute path to the SwiftFormat config file")
+  var swiftFormatConfig = Bundle.module.path(forResource: "airbnb", ofType: "swiftformat")!
+
+  @Option(help: "The absolute path to the SwiftLint config file")
+  var swiftLintConfig = Bundle.module.path(forResource: "swiftlint", ofType: "yml")!
+
+  mutating func run() throws {
+    try swiftFormat.run()
+    swiftFormat.waitUntilExit()
+
+    try swiftLint.run()
+    swiftLint.waitUntilExit()
+  }
+
+  // MARK: Private
+
+  private lazy var swiftFormat: Process = {
+    var arguments = directories + [
+      "--config", swiftFormatConfig,
+    ]
+
+    if let swiftFormatCachePath = swiftFormatCachePath {
+      arguments += ["--cache", swiftFormatCachePath]
+    }
+
+    if lint {
+      arguments += ["--lint"]
+    }
+
+    let swiftFormat = Process()
+    swiftFormat.launchPath = swiftFormatPath
+    swiftFormat.arguments = arguments
+    return swiftFormat
+  }()
+
+  private lazy var swiftLint: Process = {
+    var arguments = directories + [
+      "--config", swiftLintConfig,
+      "--in-process-sourcekit",
+    ]
+
+    if let swiftLintCachePath = swiftLintCachePath {
+      arguments += ["--cache-path", swiftLintCachePath]
+    }
+
+    if !lint {
+      arguments += ["--autocorrect"]
+    }
+
+    let swiftLint = Process()
+    swiftLint.launchPath = swiftLintPath
+    swiftLint.arguments = arguments
+    return swiftLint
+  }()
+
+}
