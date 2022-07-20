fb658f449dddcedc4c81190ee34226040673a7f9
Cal Stephens <cal@calstephens.tech>
2022-07-20 14:23:23 -0700

    Update xcode_settings.sh script to enable Xcode spell check (#171)

--- resources/xcode_settings.bash
+++ resources/xcode_settings.bash
@@ -2,6 +2,8 @@
 
 set -e
 
+defaults write com.apple.dt.Xcode AutomaticallyCheckSpellingWhileTyping -bool YES
+
 defaults write com.apple.dt.Xcode DVTTextEditorTrimTrailingWhitespace -bool YES
 defaults write com.apple.dt.Xcode DVTTextEditorTrimWhitespaceOnlyLines -bool YES
 
