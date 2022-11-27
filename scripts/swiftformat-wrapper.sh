f5321aa54c177f2e5112973acf806914209bf5e6
Iipin <52832022+Iipin@users.noreply.github.com>
2022-11-27 17:17:43 +0100

    fix: add extension to git-format-staged script & fix references in GH workflow (#6)

--- scripts/swiftformat-wrapper.sh
+++ scripts/swiftformat-wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if which swiftformat >/dev/null; then
-  scripts/git-format-staged --formatter "swiftformat --exclude Package.swift stdin --stdinpath '{}'" '*.swift'
+  scripts/git-format-staged.py --formatter "swiftformat --exclude Package.swift stdin --stdinpath '{}'" '*.swift'
 else
   echo 'warning: swiftformat not installed, run `bundle install`.'
 fi
