95c91056510381900be09292843291e5dac0b019
Cal Stephens <cal@calstephens.tech>
2025-10-17 13:38:30 -0700

    Enable `validateTestCases` SwiftFormat rule (#333)

--- Package.swift
+++ Package.swift
@@ -47,8 +47,8 @@ let package = Package(
 
     .binaryTarget(
       name: "swiftformat",
-      url: "https://github.com/calda/SwiftFormat-nightly/releases/download/2025-09-22-b/SwiftFormat.artifactbundle.zip",
-      checksum: "a5d74fe9c1a3aa0d0c8d805334bfc919ff87bc4c1ab2c77b8e17149b91b64b16"
+      url: "https://github.com/calda/SwiftFormat-nightly/releases/download/2025-10-17/SwiftFormat.artifactbundle.zip",
+      checksum: "f86382d573a5cd7a62bbbe6ab7c932aa266506498f078e5f151529283d4ac06a"
     ),
 
     .binaryTarget(
